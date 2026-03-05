143193101b08cc597a2038ac17712902b1591efb
Mnzo <81849467+Mnzo@users.noreply.github.com>
2026-03-05 16:49:32 +0100

    ish

---
+++ Source/Willow/Private/DataAssets/DicesDataAsset.cpp
@@ -0,0 +1,4 @@
+﻿// 
+
+
+#include "DataAssets/DicesDataAsset.h"
---
+++ Source/Willow/Public/DataAssets/DicesDataAsset.h
@@ -0,0 +1,23 @@
+﻿// 
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "DicesDataAsset.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class WILLOW_API UDicesDataAsset : public UPrimaryDataAsset
+{
+	GENERATED_BODY()
+	
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Visuals|UI")	
+	TArray<UTexture2D*> DicesIcons;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Visuals|Materials")
+	TArray<UMaterialInstance*> DicesMaterials;
+};
