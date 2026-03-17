a07efe8b0a9d5ccb8894b73c8a8047e63ef765c9
Mnzo <81849467+Mnzo@users.noreply.github.com>
2026-03-17 13:46:51 +0100

    big ass update

---
+++ Source/Willow/Private/DataAssets/ColorsDataAsset.cpp
@@ -0,0 +1,4 @@
+﻿// 
+
+
+#include "DataAssets/ColorsDataAsset.h"
---
+++ Source/Willow/Public/DataAssets/ColorsDataAsset.h
@@ -0,0 +1,20 @@
+﻿// 
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "ColorsDataAsset.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class WILLOW_API UColorsDataAsset : public UPrimaryDataAsset
+{
+	GENERATED_BODY()
+	
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Colors|UI")
+	FLinearColor ActiveCombinationColor;
+};
